5585107d186c74fc4e2f24e5e58b072d5907bb62
astralpunk <mooonpunk@gmail.com>
2021-03-12 00:13:52 -0800

    -A

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-// variables
-music_name = "music.mp3";
+// TODO
+/* 
+v0.1
+- able to select audio file(s) in folder and load them into playlist.
+- playlist list artist name, song name, and length
+*/
+
+// Takes selected input files and puts them in a list called files.
+// Then iterates and alerts each file name that was selected.
+//
+
+function selectFiles(e) {
+	var files = e.target.files; // FileList object
+
+	// files is a FileList of File objects. List some properties.
+	var output = [];
+	for (var i = 0, f; (f = files[i]); i++) {
+		output.push("<li><strong>", escape(f.name), "</strong> </li>");
+	}
+	document.getElementById("list").innerHTML =
+		"<ul>" + output.join("") + "</ul>";
+}
+
+document
+	.getElementById("fileSelection")
+	.addEventListener("change", selectFiles, false);
