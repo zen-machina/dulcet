733c4fd1674327ea495f34d2f26d9747b1446385
astralpunk <mooonpunk@gmail.com>
2021-03-18 15:11:34 -0700

    playPause update

--- v0.3/main.js
+++ v0.3/main.js
@@ -16,6 +16,7 @@ let fileInput = document.getElementById("fileInput");
 let playList = document.querySelector(".playlist");
 let audio = document.getElementById("audioElem");
 let audioItem = document.querySelector(".audioItem");
+let currentAudio = null;
 
 // let myfiles = fileInput.files;
 let playListFiles = [];
@@ -61,20 +62,46 @@ function addFiles() {
 	console.log(myfiles);
 }
 
+// Works for the most part.
+//Bug: when I load more songs while I have a song playing,
+// then click on new song to play, the pause botton stays on previous song.
 function audioPlayPause(e) {
 	e.preventDefault();
 
 	// checks to see if the clicked on song is currently loaded.
-	let isCurrentAudio = this.getAttribute("href") === audio.getAttribute("src");
-	if (isCurrentAudio && audio.paused) {
-		setPauseIcon(this);
+	let audioLoaded = this.getAttribute("href") === audio.getAttribute("src");
+
+	//if#1
+	if (audioLoaded && audio.paused) {
+		console.log("if#1");
+		currentAudio = this;
+		setPauseIcon(currentAudio);
 		audio.play();
-	} else if (isCurrentAudio && !audio.paused) {
-		setPlayIcon(this);
+		//elseif#2
+	} else if (audioLoaded && !audio.paused) {
+		console.log("elseif#2");
+		currentAudio = this;
+		setPlayIcon(currentAudio);
 		audio.pause();
+		//else#3
+	} else {
+		//else--if#4
+		if (currentAudio) {
+			console.log("else--if#4");
+			setPlayIcon(currentAudio);
+		}
+		console.log("else#3");
+		console.log(currentAudio);
+		currentAudio = this;
+		console.log(currentAudio);
+
+		setPauseIcon(currentAudio);
+		audio.src = currentAudio.href;
+		audio.play();
 	}
 }
 
+// (!isCurrentAudio && !audio.paused)
 function setPauseIcon(elem) {
 	let icon = elem.querySelector("i");
 	icon.classList.remove("fa-play-circle");
